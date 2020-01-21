1396f47ca7d493a36d68b32359d91e882703a0d8
siccoo <obichilaka@gmail.com>
2020-01-21 12:25:03 +0100

    Api & Components folder added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Youtube clone app</h1>
+        )
+    }
+}
+
+export default App;
