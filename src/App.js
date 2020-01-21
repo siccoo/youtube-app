545109f9e3f1481927326db0b810700671be962a
siccoo <obichilaka@gmail.com>
2020-01-21 15:24:37 +0100

    Updates on the components folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { SearchBar, VideoList, VideoDetail } from './components';
 import youtube from './api/youtube';
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Youtube clone app</h1>
+            <Grid justify="center" container spacing={16}>
+                <Grid items xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            {/* SEARCH BAR */}
+                        </Grid>
+                        <Grid item xs={8}>
+                            {/* VIDEO DETAILS */}
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/* VIDEO LIST */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
---
+++ src/components/VideoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class VideoList extends React.Component {
+    render() {
+        return(
+            <h1></h1>
+        )
+    }
+}
+
+export default VideoList;
