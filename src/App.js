8fa412d53950f4b291cead53bfe8591385498af3
siccoo <obichilaka@gmail.com>
2020-01-22 16:41:08 +0100

    Updates on some files

--- src/App.js
+++ src/App.js
@@ -5,26 +5,31 @@ import { SearchBar, VideoDetail } from './components/';
 import youtube from './api/youtube';
 
 class App extends React.Component {
+    state = {
+        video: [],
+        selectedVideo: null,
+    }
 
     handleSubmit = async (searchTerm) => {
         const response = await youtube.get('search', {
             params: { 
                 part: 'snippet',
                 maxResult: 3,
-                key: 'AIzaSyDGHw996gMiKDqpPWFOEORpYEodS3ADfSM', 
+                key: 'AIzaSyDBghoqfsf5Gp4IwZgNWSr0BJMq_UVPgdc', 
                 q: searchTerm, 
                 // [API_KEY]
             }
         });
-        // console.log(response);   
+        // console.log(response.data.items);
+        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });    
     } 
 
     render() {
         return (
             <Grid justify="center" container spacing={10}>
-                <Grid items xs={12}>
+                <Grid items xs={9}>
                     <Grid container spacing={10}>
-                        <Grid item xs={12}>
+                        <Grid item xs={9}>
                             <SearchBar onFormSubmit={this.handleSubmit} />
                         </Grid>
                         <Grid item xs={8}>
